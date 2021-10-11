334002e47ead3d8d107543287a49e7ba4c76727f
Dev1NA <86621122+Dev1NA@users.noreply.github.com>
2021-10-11 19:20:23 +0300

    Адаптив + всплывающее окно

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag = document.querySelector('.price-total');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
 
 let cost = 800;
 totalPrice = 0;
+
+menuButton.addEventListener('click', () => {
+  console.log('click');
+  menu.classList.toggle('is-open');
+});
+
 
 schemeSvg.addEventListener("click", (event) => {
   if (!event.target.classList.contains('booked')) {
